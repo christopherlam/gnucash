606df208ad664919994f391a15ff1e3359a74c61
Christopher Lam <christopher.lck@gmail.com>
2025-09-20 09:14:14 +0800

    add dconf links to host ??

--- pod.sh
+++ pod.sh
@@ -28,7 +28,12 @@ podman run -it --rm \
   -v "$PWD:/app:Z" \
   -v gnucash-build:/app/build:Z \
   -e DISPLAY=$DISPLAY \
+  -e DBUS_SESSION_BUS_ADDRESS="$DBUS_SESSION_BUS_ADDRESS" \
+  -e XDG_RUNTIME_DIR="/run/user/$UID" \
+  -e NO_AT_BRIDGE=1 \
   -v /tmp/.X11-unix:/tmp/.X11-unix:Z \
+  -v /run/user/$UID:/run/user/$UID \
+  -v /etc/machine-id:/etc/machine-id:ro \
   "$IMAGE_NAME" bash -c "
     mkdir -p /app/build
     cd /app/build
